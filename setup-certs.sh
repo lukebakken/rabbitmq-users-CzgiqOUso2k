4490adf60af3b472aa53dfcaf8048929ec8314dc
Luke Bakken <luke@bakken.io>
2022-10-18 09:09:22 -0700

    Fingers crossed

--- setup-certs.sh
+++ setup-certs.sh
@@ -9,11 +9,12 @@ readonly curdir="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
 
 readonly password='CzgiqOUso2k'
 
-readonly certs_root_dir="$curdir/tls-gen/basic/result"
-readonly ca_cert="$certs_root_dir/ca_certificate.pem"
-readonly client_cert="$certs_root_dir/client_*_certificate.pem"
-readonly client_key="$certs_root_dir/client_*_key.pem"
-readonly client_pfx="$certs_root_dir/client.pfx"
+readonly certs_root_dir="$curdir/tls-gen/basic"
+readonly certs_result_dir="$certs_root_dir/result"
+readonly ca_cert="$certs_result_dir/ca_certificate.pem"
+readonly client_cert="$certs_result_dir/client_*_certificate.pem"
+readonly client_key="$certs_result_dir/client_*_key.pem"
+readonly client_pfx="$certs_result_dir/client.pfx"
 
 rm -rf "$curdir"/*.jks "$curdir"/*.pkcs12
 
@@ -23,8 +24,14 @@ keytool -genkey -dname "cn=client-truststore" -alias client-truststore -keyalg R
 
 keytool -noprompt -import -keystore "$curdir/client-truststore.pkcs12" -storepass "$password" -trustcacerts -file "$ca_cert" -alias tls-gen_basic_ca
 
-keytool -genkey -dname "cn=client-keystore" -alias client-keystore -keyalg RSA -keystore "$(curdir)/client-keystore.pkcs12" -storetype pkcs12 -keypass "$password" -storepass "$password"
+keytool -genkey -dname "cn=client-keystore" -alias client-keystore -keyalg RSA -keystore "$curdir/client-keystore.pkcs12" -storetype pkcs12 -keypass "$password" -storepass "$password"
 
 openssl pkcs12 -export -out "$client_pfx" -passout "pass:$password" -inkey "$client_key" -in "$client_cert"
 
-keytool -importkeystore -srckeystore "$client_pfx" -srcstoretype pkcs12 -srcstorepass "$password" -destkeystore ./client-keystore.pkcs12 -destkeypass "$password" -deststorepass "$password" -deststoretype pkcs12
+keytool -importkeystore -srckeystore "$client_pfx" -srcstoretype pkcs12 -srcstorepass "$password" -destkeystore "$curdir/client-keystore.pkcs12" -destkeypass "$password" -deststorepass "$password" -deststoretype pkcs12
+
+cp -vf "$curdir/"*.pkcs12 "$curdir/producer"
+cp -vf "$curdir/"*.pkcs12 "$curdir/consumer"
+cp -vf "$certs_result_dir/ca_certificate.pem" "$curdir/rmq/certs"
+cp -vf "$certs_result_dir/server_*_certificate.pem" "$curdir/rmq/certs"
+cp -vf "$certs_result_dir/server_*_key.pem" "$curdir/rmq/certs"
